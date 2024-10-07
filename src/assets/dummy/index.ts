f61c2d4c7efcf09e46eda9db402be734648de620
Guillaume <coigniez.g@gmail.com>
2024-10-07 22:46:13 +0200

    created dummy data

---
+++ src/assets/dummy/index.ts
@@ -0,0 +1,12 @@
+import mountainLake from './backgrounds/mountain-lake.jpg';
+import logo from './logo/logo.svg';
+import logoBlack from './logo/logo-black.svg';
+import logoWhite from './logo/logo-white.svg';
+
+export const images = {
+  logo,
+  logoBlack,
+  logoWhite,
+};
+
+export { mountainLake, logo, logoBlack, logoWhite };
