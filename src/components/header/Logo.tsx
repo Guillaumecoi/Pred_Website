069494cc53d85290121f08d41957c81593af6c34
Guillaume <coigniez.g@gmail.com>
2024-10-07 15:39:24 +0200

    Splitting the header into multiple components

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,9 @@
+import { predLogo } from "../../assets";
+
+const Logo = () => (
+  <a className="block xl:mr-8 py-2" href="#home">
+    <img src={predLogo} width={45} alt="PRED" />
+  </a>
+);
+
+export default Logo;
---
+++ src/components/header/Navigation.tsx
@@ -0,0 +1,35 @@
+import { useLocation } from "react-router-dom";
+import { navigation } from "../../constants/index";
+
+interface NavigationProps {
+  openNavigation: boolean;
+  handleClick: () => void;
+}
+
+const Navigation = ({ openNavigation, handleClick }: NavigationProps) => {
+  const pathname = useLocation().pathname;
+
+  return (
+    <nav className={`${openNavigation ? 'flex fixed top-20 left-0 right-0 bottom-0 w-full' : 'hidden'}`}>
+      <div className={`relative z-2 flex flex-col lg:flex-row
+        ${openNavigation ? 'mr-auto pl-5 w-[60%] h-full' : 'ml-auto underline underline-offset-4'}`}>
+        {navigation.map((item) => (
+          <a
+            key={item.id}
+            href={item.url}
+            onClick={handleClick}
+            className={`block relative font-code text-2xl text-n-6 transition-colors hover:text-color
+              px-6 py-3 md:py-4 lg:text-base lg:font-semibold
+              ${ openNavigation ? "border-b border-gray-500 hfull p-4" : "" }
+              ${ item.url === pathname ? "z-2 lg:text-color-1" : "lg:text-n-6" }
+              lg:leading-5 xl:px-12`}
+          >
+            {item.title}
+          </a>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
