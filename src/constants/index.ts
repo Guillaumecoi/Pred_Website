451ff8f469c98eb94cd2e6fd8dc9e13e0c89c4ea
Guillaume <coigniez.g@gmail.com>
2024-10-07 22:27:44 +0200

    shortened the code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,8 @@
 // index.ts
 import { Content } from './types';
-import { content as real } from './index.content';
-import { content as dummy } from './index.dummy';
+import { content as realContent } from './index.content';
+import { content as dummyContent } from './index.dummy';
 
-let content: Content;
-
-if (process.env.USE_REAL_CONTENT === 'true') {
-  content = real;
-} else {
-  content = dummy;
-}
+const content: Content = process.env.USE_REAL_CONTENT === 'true' ? realContent : dummyContent;
 
 export const { navigation, hero, callToAction } = content;
