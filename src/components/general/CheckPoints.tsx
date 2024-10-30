60083abe5ba5eab34ca174af24abcaa0157e25d0
Guillaume <coigniez.g@gmail.com>
2024-10-30 12:09:38 +0100

    Refactor CheckPoints component so the check icon is always the same size

--- src/components/general/CheckPoints.tsx
+++ src/components/general/CheckPoints.tsx
@@ -8,9 +8,9 @@ const Checkpoints: React.FC<CheckpointProps> = ({ checks = [] }) => {
   return (
     <>
       {checks.map((check, index) => (
-        <span key={index} className="flex items-center text-left mb-2">
-          <FaCheck className="mr-2" /> 
-          {check}
+        <span key={index} className="flex items-center mb-2">
+          <FaCheck className="mr-2 flex-shrink-0" size={16} />
+          <div className="text-left">{check}</div>
         </span>
       ))}
     </>
